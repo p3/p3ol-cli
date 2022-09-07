e5bf06af9df75886b2e2c666c933f3f8b653f41d
Joe Campo <jcampo@gmail.com>
2022-09-07 05:38:14 -0400

    Display all chat rooms regardless of user count

--- app/Actions/DisplayChatRooms.php
+++ app/Actions/DisplayChatRooms.php
@@ -14,10 +14,9 @@ class DisplayChatRooms
     public function handle(ConnectionInterface $connection, Collection $rooms): void
     {
         with(new Menu('Select a chat room'), function (Menu $menu) use ($rooms, $connection) {
-            $rooms->filter(fn ($room) => $room['people'] > 0)
-                ->each(function ($room) use ($menu) {
-                    $menu->addOption($room['name'], str($room['name'])->padRight(20, ' ')."({$room['people']})");
-                });
+            $rooms->each(function ($room) use ($menu) {
+                $menu->addOption($room['name'], str($room['name'])->padRight(20, ' ')."({$room['people']})");
+            });
 
             with($menu->disableDefaultItems()->open(), function (string $name) use ($connection) {
                 LaunchChat::run($connection, $name);
